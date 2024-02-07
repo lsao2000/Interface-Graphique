1fbbf30316786cd47a3a0055491392ed8b0c6463
Yassine1666 <yassinbeakri166@gmail.com>
2024-02-07 12:36:17 +0100

    yassin commit

--- app/src/main/java/com/example/viewpagertp/MainActivity.java
+++ app/src/main/java/com/example/viewpagertp/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.viewpagertp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+ViewPager2 VP;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        VP = (ViewPager2) findViewById(R.id.viewPager);
+        PagerAdapter FA = new PagerAdapter(this );
+        VP.setAdapter(FA);
+
+
+
     }
 }
